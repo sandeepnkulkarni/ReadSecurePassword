39e54f0657e145ae48b3f089b84c40e8a1926758
Sandeep Kulkarni <Sandeep.Kulkarni4@veritas.com>
2017-11-21 11:42:29 +0530

    Added filelogger numbers

--- build.bat
+++ build.bat
@@ -8,6 +8,6 @@ set Config=%~2
 SET LogFile=%~dp0compile_%Action%.log
 SET ErrLogFile=%~dp0compile_%Action%_errors.log
 @ECHO ON
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBUILD.EXE" /m /nologo "%~dp0ReadSecurePassword.sln" /p:Configuration=%Config% /p:Platform="Any CPU" /t:%Action% /flp:"LogFile=%LogFile%" /flp:"errorsonly;LogFile=%ErrLogFile%"
+"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBUILD.EXE" /m /nologo "%~dp0ReadSecurePassword.sln" /p:Configuration=%Config% /p:Platform="Any CPU" /t:%Action% /flp1:"LogFile=%LogFile%" /flp2:"errorsonly;LogFile=%ErrLogFile%"
 @ECHO.
 @ECHO Build log file: %LogFile%
